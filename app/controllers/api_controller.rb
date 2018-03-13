c096a6dde989dd69e4e66df1b59ad1fe065a4783
phundi <kennethkapundi1@gmail.com>
2018-03-13 16:12:13 +0200

    Added a root page to view posssible anomalies

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,9 @@
 class ApiController < ActionController::Base
-  
+
+  def home
+    @current_count = PersonBirthDetail.select(" COUNT(national_serial_number) AS c ").where(" national_serial_number IS NOT NULL")[0]['c'].to_i rescue 0
+  end
+
   def start_sync
     SyncCheck.perform_in(1)
     render :text => 'ok'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'api#home'
   get "/api/start_data_loading"
   get "/api/start_ping"
   get "/api/start_sync"
+  
 end
